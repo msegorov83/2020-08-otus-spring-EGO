fc355a7f15c710114a84ea897c4a33842675e2e8
msegorov <msegorov@stalcom.com>
2020-11-05 10:42:36 +0300

    homework03

--- TestStudents03/src/main/java/ru/otus/spring/Main.java
+++ TestStudents03/src/main/java/ru/otus/spring/Main.java
@@ -3,15 +3,8 @@ package ru.otus.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.core.io.Resource;
 import ru.otus.spring.config.AppProps;
 import ru.otus.spring.runner.InitApp;
-import ru.otus.spring.service.ExamService;
-import ru.otus.spring.service.QuestionService;
-import ru.otus.spring.service.StudentService;
-
-import java.util.Locale;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProps.class)
